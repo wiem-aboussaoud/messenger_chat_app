0a31cd2353cf33b32f894a7b2e358b0fc9322dde
wiem-aboussaoud <114400104+wiem-aboussaoud@users.noreply.github.com>
2022-11-19 23:04:42 +0100

    Update settings.py

--- MessengerApp/settings.py
+++ MessengerApp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',  # add the rest framework to the installed apps
     "rest_framework.authtoken",
+    "corsheaders",
     "ProfileManager",
 ]
 
