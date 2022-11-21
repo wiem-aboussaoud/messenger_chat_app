f8c62448633081ff8a0168f22c065bebed042e45
wiem-aboussaoud <114400104+wiem-aboussaoud@users.noreply.github.com>
2022-11-21 12:51:22 +0100

    Add files via upload

--- MessengerApp/settings.py
+++ MessengerApp/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = "django-insecure-t1__gq6+^+!2atkvl!b+!op=#(ixg$$e&kdanlhpj7gka@!c_0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["messenger-chat-app-wiem.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
